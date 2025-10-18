e534df174b776da4240baf6d134af74003a9da3a
Vitaliy Dimov <v@forij.dev>
2025-10-19 01:14:14 +0300

    change-prod-path-v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const prodBasePath = '/test-task-basic-image-transformer-open/';
+const prodBasePath = '/basic-image-transformer-open/';
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
